102d219fec0a1da74f2dbaf67628287516e0625b
First Last <makwana.dashang@gmail.com>
2021-04-25 07:08:13 +0000

    first commit

---
+++ HelloJava.java
@@ -0,0 +1,11 @@
+va.java in the terminal and add the below code in HelloJava.java file:
+
+class HelloJava {
+
+public static void main(String args[]){
+
+System.out.Println(“ I am your Java Program. Thank you! ”);
+
+
+
+}
